670204087f7f815972569465a7629fa0ce8a6b6e
CotsaftisGarnierMatthieu <git-commit-address>
2025-04-22 13:22:58 +0200

    fix : trying to determinate the steps per radian

--- zlannch_servoing2cam.sh
+++ zlannch_servoing2cam.sh
@@ -4,4 +4,4 @@ set -e
 
 source install/setup.bash
 
-exec ros2 launch robot_control_tb visual_control.launch.py num_cameras:=2
+exec ros2 launch robot_control_tb visual_control.launch.py num_cameras:=2 open_loop:=false
